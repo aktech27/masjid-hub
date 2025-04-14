335edcf17621705a404330d276dc21c05fde081c
Ahamed Kabeer <ak.tech.27.coder@gmail.com>
2025-04-14 12:40:55 +0530

    MUI and React Router added

---
+++ client/src/routes/Home.tsx
@@ -0,0 +1,11 @@
+import Typography from "@mui/material/Typography";
+
+const Home = () => {
+  return (
+    <Typography variant="h1" component="h2">
+      Hello World
+    </Typography>
+  );
+};
+
+export default Home;
