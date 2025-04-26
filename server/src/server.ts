f14b81bae5f1a5f936670c18d84049d6b08ff069
Ahamed Kabeer <ak.tech.27.coder@gmail.com>
2025-04-26 22:25:41 +0530

    DB config with drizzle added

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,14 +2,17 @@ import "module-alias/register";
 import "dotenv/config";
 import http from "http";
 import app from "./app";
+import db from "./db";
+import { testTable } from "./db/schema";
 
 import logger from "@config/logger";
 import CONSTANTS from "@config/index";
 
 const server = http.createServer(app);
 
-server.listen(CONSTANTS.PORT, () => {
+server.listen(CONSTANTS.PORT, async () => {
   logger.info(
     `[${CONSTANTS.ENV}] Server is up and running on port: ${CONSTANTS.PORT}`
   );
+  const res = await db.$count(testTable);
 });
